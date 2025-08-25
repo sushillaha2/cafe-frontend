54280f0a888d94e54e7a3961b3e542b748f5610d
sushillaha2 <sushillaha2@gmail.com>
2025-08-26 00:13:55 +0530

    Second commit

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,24 @@
-// src/api/axios.js
-import axios from 'axios';
+// // src/api/axios.js
+// import axios from 'axios';
+
+// export default axios.create({
+//   baseURL: 'http://localhost:5000',
+// });
+
+
+
+
+
+import axios from "axios";
+
+// Vite ke env variable se API base URL lo
+const API_BASE = import.meta.env.VITE_API_BASE;
+
+console.log("API BASE URL:", API_BASE);
 
 export default axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: API_BASE,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
